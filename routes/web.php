5a17ff48e3ab9a36c4caf9b101e70e534f689551
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-26 15:21:43 +0200

    -Hello World-

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $Hello = [
+        'Hello' => 'Hello World',
+        'Name' => 'Piergiorgio Piras',
+        'Class' => 'Classe 56',
+    ];
+    return view('home', $Hello);
 });
